278931684df389c23326c12d8a711ed2938e9f65
Damis-faith <111054555+Damis-faith@users.noreply.github.com>
2022-09-13 16:02:55 +0100

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
  * main - Check description
  * Description: It prints the word Holberton, followed by a new line.
@@ -10,7 +9,7 @@ int main(void)
 {
 	char word[9] = "_putchar";
 	int i;
-	
+
 	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 	_putchar('\n');
